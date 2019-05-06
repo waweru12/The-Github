fe8efe60e4a02ee1472c162660d1e7b7ff812c4a
savannah8 <willengitonga.wg@gmail.com>
2019-05-06 12:21:36 +0300

    generated class repos and github.service

---
+++ src/app/github.service.spec.ts
@@ -0,0 +1,7 @@
+import { Github.Service } from './github.service';
+
+describe('Github.Service', () => {
+  it('should create an instance', () => {
+    expect(new Github.Service()).toBeTruthy();
+  });
+});
