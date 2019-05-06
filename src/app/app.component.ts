2fcee4f9c483ab5b3dc93e2651a509a6d712bacb
savannah8 <willengitonga.wg@gmail.com>
2019-05-06 15:34:43 +0300

    profile html details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { GithubService } from './github.service';
 import { ProfileComponent } from './profile/profile.component';
 
 @Component({
