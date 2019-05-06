87d44a3671c73501ab5290fdb6d8f9631b7dc56c
savannah8 <willengitonga.wg@gmail.com>
2019-05-06 17:11:45 +0300

    app.css jumbotron styles

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { User } from './user';
+import { Repos } from './repos';
 import { environment } from '../environments/environment';
 
 
@@ -8,6 +9,7 @@ import { environment } from '../environments/environment';
 @Injectable()
 export class GithubService {
     username: string;
+    private apikey :string ="2d22f1521e6a76e215b0ec1a11816c901f33e496"
 
     constructor(private http: HttpClient) {
         console.log('Github Service Ready...');
@@ -15,11 +17,11 @@ export class GithubService {
 
     }
     getUser() {
-        return this.http.get('https://api.github.com/users/' + this.username);
+        return this.http.get('https://api.github.com/users/' + this.username+"?access_token="+this.apikey)
 
     }
     getRepos() {
-        return this.http.get(' https://api.github.com/users/' + this.username + '/repos');
+        return this.http.get('https://api.github.com/users/' + this.username + "/repos" + "?access_token"+this.apikey)
     }
     updateUser(username: string) {
         this.username = username;
